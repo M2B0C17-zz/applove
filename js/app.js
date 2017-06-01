513dc5a947a7ea99ccfaa9fab9fe93e25c1098c4
macarena baltra <macarena.bc2011@gmail.com>
2017-05-31 22:16:29 -0400

    Subiendo Ejercicio EV1 - Cambios en JS

--- js/app.js
+++ js/app.js
@@ -26,27 +26,27 @@
 	});
 })();
 
-// Modal 2
+// Modal 2 seccion 3
 
-(function mDos(){
-	var boxes2 = Array.from(document.getElementsByClassName("col-4 foto")); //revisar si tira error
-	var modal2 = document.getElementById("modalDosBox");
-	var bodyModal2, close, img;
-	boxes2.forEach(function(box2){
-		box2.addEventListener("click", function(){
+(function modelDos(){
+	var cajas2 = Array.from(document.getElementsByClassName("col-4 foto")); // recorremos el arreglo de los div que tienen la class col-4 foto
+	var modal2 = document.getElementById("modalDosBox"); // conectamos con el html
+	var cuerpitoModal2, close, img;
+	cajas2.forEach(function(box2){ // recorremos los elementos nuevos que creamos abajo
+		box2.addEventListener("click", function(){ // agregamos el evento de cuando se presiona con el mouse sobre el elemento
 			modal2.innerHTML = "";
-			bodyModal2 = document.createElement("div");
-			bodyModal2.classList.add("modal-body2");
-			bodyModal2.innerHTML = box2.innerHTML;
-			modal2.appendChild(bodyModal2);
+			cuerpitoModal2 = document.createElement("div"); // creamos un div + una class
+			cuerpitoModal2.classList.add("modal-cuerpito2"); // esto lo usamos despues en el CSS
+			cuerpitoModal2.innerHTML = box2.innerHTML;
+			modal2.appendChild(cuerpitoModal2);
 			modal2.classList.remove("hide2");
-			close = document.createElement("div");
-			close.classList.add("close");
-			img = document.createElement("img");
-			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close = document.createElement("div"); // creamos un div + class de para cerrar
+			close.classList.add("close"); // esto lo usamos en CSS
+			img = document.createElement("img"); // creamos un etiqueta img para agregar el icono de la X
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png"); // url del icono a usar
 			close.appendChild(img);
 			modal2.appendChild(close);
-			close.addEventListener("click",function(){
+			close.addEventListener("click",function(){ // evento que decimos que al posecionarse sobre el elemento X cerrar, en este caso model 2
 				modal2.classList.add("hide2");
 			});
 		});
